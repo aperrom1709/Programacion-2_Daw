26e6ebad400163f3d02431b10e57678099e74098
aperrom1709 <151721555+aperrom1709@users.noreply.github.com>
2025-10-27 13:31:04 +0100

    Add files via upload

--- Ejercicios phyton 2/Arryas_SumaRecorrido.py
+++ Ejercicios phyton 2/Arryas_SumaRecorrido.py
@@ -1,6 +1,10 @@
 numeros = [1, 2, 3, 4, 5]
 
-for numero in numeros:
-    print(numero)
-i = 0
+for i in numeros:
+    print(i)
+    
+suma = 0
+for i in numeros:
+    suma += i  
 
+print("La suma de los números es:", suma)
